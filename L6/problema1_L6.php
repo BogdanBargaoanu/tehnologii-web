ac601eb1300150f27a7bbd6550f6e963236febbe
BogdanBargaoanu <bn19bba@yahoo.com>
2025-05-20 23:33:42 +0300

    p1l6

---
+++ L6/problema1_L6.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lucrare laborator PHP</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            border: 1px solid blue;
+            padding: 10px;
+            margin: 20px;
+            width: 240px;
+        }
+        h3 {
+            color: blue;
+            margin-top: 0;
+        }
+        p {
+            color: blue;
+            margin: 5px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <?php
+        echo "<h3>Lucrare laborator PHP</h3>";
+        echo "<p>PHP - Acesta este primul meu script PHP</p>";
+        echo "<p>PHP - Acesta este cel de al doilea script PHP</p>";
+        ?>
+    </div>
+</body>
+</html>
